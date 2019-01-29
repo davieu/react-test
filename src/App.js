155504a2d980ea0ea3df8bfdcf3885382df6b8ec
davieu <davisumana@gmail.com>
2019-01-29 11:09:45 -0500

    added click handler

--- src/App.js
+++ src/App.js
@@ -7,9 +7,22 @@ class App extends Component {
   state = {
     persons: [
       { name: 'Max', age: 28 },
-      { name: 'Manu', qge: 29 },
+      { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 26 }
-    ]
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = () => {
+    //console.log('Was clicked!');
+    //DON'T DO THIS- won't work when you change state directly like this  this.state.persons[0].name = 'Maximilian'
+    this.setState({ 
+      persons: [
+        { name: 'Maximilian', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    })
   }
 
   render() {
@@ -17,21 +30,28 @@ class App extends Component {
       <div className="App">
         <h1>Hi I'm a react app</h1>
         <p>This is really working!</p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
           {/*you can render them both ways. Bottom is for nesting stuff in between. props.children is how you get the elements within the opening and closing tags of the component */}
         {/* 
         <Person name='Max' age='28'/>
         <Person name='Manu' age='29'><p>My Hobbies: Racing</p></Person>
         <Person name='Stephanie' age='26'><p>test: kayak</p></Person>
         */}
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-
-
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}><p>My Hobbies: Racing</p></Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}><p>test: kayak</p></Person>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}><p>
+          My Hobbies: Racing</p></Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
       </div>
   
     );
+
+    //this is react without jsx
     // return React.createElement('div', {className: 'App'}, null, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
   }
 }
